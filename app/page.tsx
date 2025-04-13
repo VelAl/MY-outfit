1ebb89760b5e798d4a97351bb84eedd881dfccaa
Alex <welmozhko88@gmail.com>
2025-04-13 15:49:36 +0300

    set up shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
-    <div>page</div>
-  )
-}
-export default HomePage
+    <div>
+      <Button className="m-4">Click me</Button>
+    </div>
+  );
+};
+export default HomePage;
