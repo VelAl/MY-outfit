b7ed096623ba13b1f6aa8566f1689732f0cdf71c
Alex <welmozhko88@gmail.com>
2025-04-14 16:47:35 +0300

    set up product card. Basic structure

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 
 import { T_Product } from "@/lib/appTypes";
 
+import ProductCard from "./product-card";
+
 type I_Props = {
   data: T_Product[];
   title?: string;
@@ -15,10 +17,10 @@ const ProductList: FC<I_Props> = ({ data, title, limit }) => {
     <div className="my-10">
       <h2 className="h2-bold mb-4">{title}</h2>
 
-      {data.length ? (
+      {!!data.length ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product, i) => (
-            <div key={i}>{product.name}</div>
+          {limitedData.map((product) => (
+            <ProductCard key={product.slug} product={product} />
           ))}
         </div>
       ) : (
