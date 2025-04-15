e7cda6158707d0f5040229397a500e66a1e01fe9
Alex <welmozhko88@gmail.com>
2025-04-15 23:26:11 +0300

    set up neondatabase/serverless & prisma/adapter-neon. Regenerated prisma

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,17 +1,13 @@
 "use server";
-import { T_Product } from "@/app-types-ts";
+import { prisma } from "@/db/prisma";
 
 import { LATEST_PRODS_LIMIT } from "../constants";
-import { PrismaClient, Product } from "../generated/prisma";
-import { converЕoToJSObj } from "../utils";
 
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
-  return converЕoToJSObj<T_Product[], Product[]>(data);
+  return data;
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,5 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-//Convert prisma obj into a regular JS obj
-export const converЕoToJSObj = <T, K>(value: K) =>
-  JSON.parse(JSON.stringify(value)) as T;
-
 //Format number with decimals
 export const fomatNumWithDecimals = (num: number) => num.toFixed(2);
