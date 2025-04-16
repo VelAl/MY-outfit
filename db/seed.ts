6e438e2f99705b245fca134d61e4e079ae2d7f5b
Alex <welmozhko88@gmail.com>
2025-04-16 15:14:04 +0300

    seed.ts file fix prisma import

--- db/seed.ts
+++ db/seed.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 import { sampleData } from "./sample-data";
 
 async function main() {
-    const prisma = new PrismaClient()
-    await prisma.product.deleteMany()
+  const prisma = new PrismaClient();
+  await prisma.product.deleteMany();
 
+  await prisma.product.createMany({ data: sampleData.products });
 
-    await prisma.product.createMany({data: sampleData.products})
-
-    console.log('DB has been seeded successfully!');
-    
+  console.log("DB has been seeded successfully!");
 }
 
-main()
+main();
