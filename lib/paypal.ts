e980acc5772607585e526e1ec19566c7ed9077bc
Alex <welmozhko88@gmail.com>
2025-04-24 17:09:56 +0300

    paypal createOrder & capturePayment mrthods. Tests

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,57 @@
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+const handleResponse = async (response: Response) => {
+  if (response.ok) {
+    return response.json();
+  } else {
+    const errMsg = await response.text();
+    throw new Error(errMsg);
+  }
+};
+
+export const paypal = {
+  async createOrder(price: number) {
+    const token = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: {
+              currency_code: "USD",
+              value: price,
+            },
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(response);
+  },
+
+  async capturePayment(orderId: string) {
+    const token = await generateAccessToken();
+
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return handleResponse(response);
+  },
+};
 
 // generate paypal an access token
 const generateAccessToken = async () => {
@@ -19,13 +70,8 @@ const generateAccessToken = async () => {
     },
   });
 
-  if (response.ok) {
-    const jsnData = await response.json();
-    return jsnData.access_token;
-  } else {
-    const errorMessage = await response.text();
-    throw new Error(errorMessage);
-  }
+  const jsnData = await handleResponse(response);
+  return jsnData.access_token;
 };
 
 export { generateAccessToken };
--- tests/paypal.test.ts
+++ tests/paypal.test.ts
@@ -1,9 +1,37 @@
-import { generateAccessToken } from "../lib/paypal";
+import { generateAccessToken, paypal } from "../lib/paypal";
 
-// test to jenerate access token from paypal
+// jenerate access token from paypal test
 test("generates token from paypal", async () => {
   const tokenResponse = await generateAccessToken();
 
   expect(typeof tokenResponse).toBe("string");
   expect(tokenResponse.length).toBeGreaterThan(0);
+}, 1e4);
+
+// paypal order creation test
+test("creates a paypal order", async () => {
+  const price = 10.0;
+
+  const orderResponse = await paypal.createOrder(price);
+
+  expect(orderResponse).toHaveProperty("id");
+  expect(orderResponse).toHaveProperty("status");
+  expect(orderResponse.status).toBe("CREATED");
+});
+
+// capture payment with mock order test
+test("simulating capturing a payment from an order", async () => {
+  const orderId = "100";
+
+  const mockCapturePayment = jest
+    .spyOn(paypal, "capturePayment")
+    .mockResolvedValue({
+      status: "COMPLETED",
+    });
+
+  const captureResponse = await paypal.capturePayment(orderId);
+
+  expect(captureResponse).toHaveProperty("status", "COMPLETED");
+
+  mockCapturePayment.mockRestore();
 });
